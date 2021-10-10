a829a46a3f58b4bd628c738da67c1bed762530a6
Will Martinez <will.martinez.avellan@gmail.com>
2021-10-09 20:40:19 -0600

    Print options of fmt package

--- src2/main.go
+++ src2/main.go
@@ -7,5 +7,21 @@ func main() {
 	helloMessage := "Hello"
 	worldMessage := "World"
 
+	// Println
 	fmt.Println(helloMessage, worldMessage)
+	fmt.Println(helloMessage, worldMessage)
+
+	// Printf
+	nombre := "Platzi"
+	cursos := 500
+	fmt.Printf("%s has more than %d cursos\n", nombre, cursos)
+	fmt.Printf("%v has more than %v cursos\n", nombre, cursos)
+
+	// Sprintf
+	message := fmt.Sprintf("%s has more than %d cursos", nombre, cursos)
+	fmt.Println(message)
+
+	// data Type
+	fmt.Printf("helloMessage: %T\n", helloMessage)
+	fmt.Printf("cursos: %T\n", cursos)
 }
