3c4a1dc0b01b9185149e9d45e27d0c3968036e82
Will Martinez <will.martinez.avellan@gmail.com>
2021-11-07 22:07:24 -0600

    go routines

---
+++ goroutines/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func say(text string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	fmt.Println(text)
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	fmt.Println("Hello")
+	wg.Add(1)
+
+	go say("world", &wg)
+
+	wg.Wait()
+
+	go func(text string) {
+		fmt.Println(text)
+	}("Bye")
+
+	time.Sleep(time.Second * 1)
+}
